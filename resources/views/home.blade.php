df0774e378a1ca9a6f1c1047cd6e91d1a1340407
Dennis <triaxxin29@gmail.com>
2024-06-06 20:20:12 -0500

    fourth push +1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     </x-slot:heading>
 {{--    <h1><strong>{{$greeting}} Mutha Fucka!! My name is {{$name}}</strong></h1>--}}
 
-
+    <div class="flex -space-x-1 overflow-hidden">
+        <img class="inline-block h-15 w-15 rounded-full ring-2 ring-white" src="https://variety.com/wp-content/uploads/2019/07/the-mask.jpg?w=1000&h=585&crop=1" alt="">
+    </div>
 
 </x-layout>
