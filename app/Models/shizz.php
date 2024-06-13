a461d8b1398c01ad6c5a5cae3ecc92f9d14afeb8
Dennis <triaxxin29@gmail.com>
2024-06-12 20:49:04 -0500

    trying again because some items are showing as not having gone up yet

--- app/Models/shizz.php
+++ app/Models/shizz.php
@@ -5,8 +5,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class shizz extends Model{
+    use HasFactory;
+
     protected $table = 'table_stupid_data';
 
     protected $fillable =['name','email', 'phone', 'is_stupid'];
